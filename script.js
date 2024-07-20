2321136d717806ebb4fa07e4c0e7b9dd263c76ac
Marvelous Osondu <marvnonso554@gmail.com>
2024-07-20 08:27:51 +0100

    Update styles

---
+++ script.js
@@ -0,0 +1,6 @@
+const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
